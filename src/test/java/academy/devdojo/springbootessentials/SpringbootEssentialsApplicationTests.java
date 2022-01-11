245b94c7c50dc6875b9a98fbb09a1df9b09ada15
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-01-11 14:01:08 -0300

    Generating project with spring initializer

---
+++ src/test/java/academy/devdojo/springbootessentials/SpringbootEssentialsApplicationTests.java
@@ -0,0 +1,15 @@
+package academy.devdojo.springbootessentials;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class SpringbootEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
